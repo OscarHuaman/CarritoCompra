3803b97024d3f894c939439a4c0d79549ef8db71
SOPORTE-01 <SOPORTE-01@SOPORTE02-NESTLE>
2017-07-07 11:51:55 -0500

    a1

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -17,7 +17,6 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         System.out.println("PRUEBA2");
         System.out.println("Prueba");
-        System.out.println("OSCAR");
     }
           
 }
